25744620fc1df83ab4316b8c2136a33dd0d9a194
Okan Konur <okan.konur@gmail.com>
2022-12-17 02:26:03 +0300

    updated lua config

--- after/plugin/treesitter.lua
+++ after/plugin/treesitter.lua
@@ -1,13 +1,13 @@
 require'nvim-treesitter.configs'.setup {
   -- A list of parser names, or "all"
-  ensure_installed = {"javascript", "typescript", "c", "c_sharp", "cpp", "java", "python", "lua"}
+  ensure_installed = {"javascript", "typescript", "c", "c_sharp", "cpp", "java", "python", "lua"},
 
   -- Install parsers synchronously (only applied to `ensure_installed`)
   sync_install = false,
 
   -- Automatically install missing parsers when entering buffer
   -- Recommendation: set to false if you don't have `tree-sitter` CLI installed locally
-  auto_install = true,
+  auto_install = false,
 
   highlight = {
     -- `false` will disable the whole extension
--- lua/okank/packer.lua
+++ lua/okank/packer.lua
@@ -1,7 +1,5 @@
 -- This file can be loaded by calling `lua require('plugins')` from your init.vim
 
--- Only required if you have packer configured as `opt`
-vim.cmd [[packadd packer.nvim]]
 
 return require('packer').startup(function(use)
   -- Packer can manage itself
@@ -10,7 +8,7 @@ return require('packer').startup(function(use)
   use {
 	  'nvim-telescope/telescope.nvim', tag = '0.1.0',
 	  -- or                            , branch = '0.1.x',
-	  requires = { {'nvim-lua/plenary.nvim'} }   
+	  requires = { {'nvim-lua/plenary.nvim'} } 
   }
 
   use('morhetz/gruvbox')
