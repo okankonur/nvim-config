07f00b8c918fd26abd16207aa2ef9012d6ab4c5e
Okan Konur <okan.konur@gmail.com>
2022-12-17 02:11:06 +0300

    updated lua config

---
+++ lua/okank/keymap.lua
@@ -0,0 +1,5 @@
+vim.g.mapleader = " "
+
+-- opens netrw directory explorer
+vim.keymap.set("n", "<leader>qq", vim.cmd.Ex)
+
---
+++ lua/okank/set.lua
@@ -0,0 +1,14 @@
+vim.opt.relativenumber = true
+
+
+vim.opt.swapfile = false
+vim.opt.backup = false
+vim.opt.undodir = os.getenv("HOME") .. "/.vim/undodir"
+vim.opt.undofile = true
+
+
+vim.opt.termguicolors = true
+
+vim.opt.scrolloff = 8
+vim.opt.signcolumn = "yes"
+vim.opt.isfname:append("@-@")
