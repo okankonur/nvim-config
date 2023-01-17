a4fe629e1c65e793bf9eb7ecb1d02d8652df847c
Okan Konur <okan.konur@gmail.com>
2023-01-17 23:56:21 +0300

    added previous and next buffer switch

--- lua/okank/keymap.lua
+++ lua/okank/keymap.lua
@@ -3,3 +3,6 @@ vim.g.mapleader = " "
 -- opens netrw directory explorer
 vim.keymap.set("n", "<leader>qq", vim.cmd.Ex)
 
+-- switch to previous and next buffer
+vim.keymap.set("n", "<leader>b", ":bn<CR>")
+vim.keymap.set("n" ,"<leader>n", ":bp<CR>")
